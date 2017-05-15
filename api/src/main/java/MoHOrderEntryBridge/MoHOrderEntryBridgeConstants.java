514af6a7ac7b443f8b3ea7661ff513fa7700e1e3
k-joseph <kaweesi@jembi.org>
2017-05-14 23:08:28 -0400

    RWSPU-95 peer educator and phone number person attribute constants

---
+++ api/src/main/java/MoHOrderEntryBridge/MoHOrderEntryBridgeConstants.java
@@ -0,0 +1,9 @@
+package MoHOrderEntryBridge;
+
+public class MoHOrderEntryBridgeConstants {
+	public static String PERSONATTRIBUTE_UUID_PHONENUMBER = "43bb744c-3914-11e7-a919-92ebcb67fe33";
+
+	public static String PERSONATTRIBUTE_UUID_PEER_EDUCATOR_NAME = "43bb76cc-3914-11e7-a919-92ebcb67fe33";
+
+	public static String PERSONATTRIBUTE_UUID_PEER_EDUCATOR_PHONENUMBER = "43bb77c6-3914-11e7-a919-92ebcb67fe33";
+}
